5a272db5b0cc80d1e41ae2c42c1917bb4aef2910
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-01 16:22:48 -0300

    Created AnimatorSetter

---
+++ Assets/Scripts/AnimatorSetter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Pathfinding;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class AnimatorSetter : MonoBehaviour
+{
+    [SerializeField] private Animator anim;
+    [SerializeField] private AIPath pathAI;
+    [SerializeField] private Unit unit;
+    
+    private void Awake()
+    {
+        if (!anim)
+        {
+            anim = GetComponent<Animator>();
+        }
+
+        if (!pathAI)
+        {
+            pathAI = GetComponent<AIPath>();
+        }
+        
+        if (!unit)
+        {
+            unit = GetComponent<Unit>();
+        }
+    }
+
+    void Update()
+    {
+        if (pathAI.hasPath)
+        {
+            anim.SetBool("Walking", true);
+        }
+        else
+        {
+            anim.SetBool("Walking", false);
+        }
+    }
+}
