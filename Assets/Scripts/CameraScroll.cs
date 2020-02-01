0bbf2864b4dc96a4f6c98884a53c23d69af7b144
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-01 11:31:10 -0300

    Trying to get finger number 2 release

--- Assets/Scripts/CameraScroll.cs
+++ Assets/Scripts/CameraScroll.cs
@@ -2,14 +2,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
+using TMPro;
 using UnityEngine;
 using Vector2 = UnityEngine.Vector2;
 using Vector3 = UnityEngine.Vector3;
+using UnityEngine.InputSystem.EnhancedTouch;
 using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class CameraScroll : MonoBehaviour
 {
     [SerializeField] private Transform camTransform = null;
+    [SerializeField] private TextMeshProUGUI T1text, T2text, sText;
+    [SerializeField] private Camera cam;
+    
+    
+    private float initialDistance, bufferDistance, initialSize;
+    private Vector2 t1Pos, t2Pos;
+    private bool canPinch;
+    private bool beginPinch;
 
     private Inputs _inputs;
     private Vector2 bufferVector, mouseBufferVector;
@@ -17,6 +27,7 @@ public class CameraScroll : MonoBehaviour
 
     private void Awake()
     {
+        EnhancedTouchSupport.Enable();
         _inputs = new Inputs();
         if (!camTransform)
         {
@@ -31,6 +42,39 @@ public class CameraScroll : MonoBehaviour
 
         _inputs.MouseMovement.MousePress.performed += ctx => canMouse = true;
         _inputs.MouseMovement.MousePress.canceled += ctx => canMouse = false;
+        
+        
+        _inputs.Pinch.Touch1.performed += ctx =>
+        {
+            t1Pos = ctx.ReadValue<Vector2>();
+        };
+        _inputs.Pinch.Touch2.performed += ctx =>
+        {
+            if (canPinch)
+            {
+                t2Pos = ctx.ReadValue<Vector2>();
+                bufferDistance = (Vector2.Distance(t1Pos, t2Pos) - initialDistance);
+                cam.orthographicSize = initialSize - bufferDistance / 100;
+                sText.SetText(bufferDistance.ToString());
+            }
+        };
+        /*_inputs.Pinch.Tap2.canceled += ctx =>
+        {
+            canPinch = false; 
+            sText.SetText("NOT PINCHING");
+        };*/
+
+        if (!cam)
+        {
+            try
+            {
+                cam = GetComponent<Camera>();
+            }
+            catch (Exception e)
+            {
+                Debug.Log(e);
+            }
+        }
     }
 
     private void Update()
@@ -38,6 +82,20 @@ public class CameraScroll : MonoBehaviour
         camTransform.position += ((Vector3) (-bufferVector/6) * Time.deltaTime);
         if(canMouse)
             MouseMoveCamera(mouseBufferVector/6);
+        if (Touch.activeFingers.Count < 2)
+        {
+            canPinch = false; 
+            sText.SetText("NOT PINCHING");
+        }
+        else
+        {
+            if (!canPinch)
+            {
+                initialDistance = Vector2.Distance(t1Pos, t2Pos);
+                initialSize = cam.orthographicSize;
+                canPinch = true;
+            }
+        }
     }
 
     private void MouseMoveCamera(Vector3 deltaVector)
@@ -48,10 +106,12 @@ public class CameraScroll : MonoBehaviour
     private void OnEnable()
     {
         _inputs.Movement.Enable();
+        _inputs.Pinch.Enable();
     }
 
     private void OnDisable()
     {
         _inputs.Movement.Disable();
+        _inputs.Pinch.Disable();
     }
 }
