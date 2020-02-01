7cc1864da391aeb24119c9af75ca6eaba4a12ea2
Will <will.lucen4@gmail.com>
2020-02-01 00:53:53 -0300

    Draw a path with line renderer based on target points

---
+++ Assets/Scripts/PathCreator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class PathCreator : MonoBehaviour
+{
+    [SerializeField] private List<Transform> checkpoints;
+    private LineRenderer _lineRenderer;
+
+    [SerializeField] private bool loadManually;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!loadManually)
+        {
+            loadPoints();
+        }
+        drawLines();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void loadPoints()
+    {
+        checkpoints = transform.GetComponentsInChildren<Transform>().ToList();
+        checkpoints.RemoveAt(0);
+    }
+    
+    private void drawLines()
+    {
+        GameObject line = new GameObject();
+
+        _lineRenderer = line.AddComponent<LineRenderer>();
+        _lineRenderer.startWidth = 0.1f;
+        _lineRenderer.endWidth = 0.1f;
+        _lineRenderer.positionCount = this.checkpoints.Count;
+
+        Vector3[] checkpoints = new Vector3[this.checkpoints.Count];
+
+        for (int i = 0; i < this.checkpoints.Count; i++)
+        {
+            checkpoints[i] = new Vector3(this.checkpoints[i].position.x, this.checkpoints[i].position.y, 0f);
+        }
+        
+        _lineRenderer.SetPositions(checkpoints);
+    }
+}
