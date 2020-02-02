6f25b3385fb394b1c3792761995b2b81ac750cd7
Will <will.lucen4@gmail.com>
2020-02-02 11:37:17 -0300

    New sprites added, animations and ux improved

---
+++ Assets/Scripts/AnimationDefinition.cs
@@ -0,0 +1,13 @@
+﻿  
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "mew animation", menuName = "Animations")]
+public class AnimationDefinition : ScriptableObject
+{
+    [Range(0.1f, 5f)]
+    [SerializeField] private float _duration;
+    [SerializeField] private AnimationCurve _curve;
+    
+    public float duration => _duration;
+    public AnimationCurve curve => _curve;
+}
