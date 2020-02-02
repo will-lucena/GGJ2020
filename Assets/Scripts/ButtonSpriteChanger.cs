ca513a2a28c12e95347f7d5310b75791922b7ebf
lucena-fr4ct1ons <gabriellucena23@gmail.com>
2020-02-02 10:14:02 -0300

    Added menu animations

---
+++ Assets/Scripts/ButtonSpriteChanger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSpriteChanger : MonoBehaviour
+{
+    [SerializeField] private Button button;
+    [SerializeField] private Image imageToChange;
+
+    [SerializeField] private Sprite regularSprite, pressedSprite;
+
+    public void ChangeSprite()
+    { 
+        StartCoroutine(SpriteChange());
+    }
+
+    private IEnumerator SpriteChange()
+    {
+        imageToChange.sprite = pressedSprite;
+        yield return new WaitForSeconds(0.5f);
+        imageToChange.sprite = regularSprite;
+    }
+}
